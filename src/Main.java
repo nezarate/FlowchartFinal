b3b9fa9c2e976c807511f553a1aa0fe03ac7789d
nezarate <91988363+nezarate@users.noreply.github.com>
2023-03-17 19:53:16 -0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
 
@@ -10,25 +11,52 @@ public class Main extends JFrame {
 
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu ("File");
+        JMenu menuHelp = new JMenu("Help");
         JMenuItem clear = new JMenuItem ("Clear");
         JMenuItem save = new JMenuItem ("Save file");
         JMenuItem load = new JMenuItem ("Load file");
 
+        JMenu shapes = new JMenu("Shapes");
+        JMenuItem method = new JMenuItem("Call a Method");
+        JMenuItem instruction = new JMenuItem("Instruction");
+        JMenuItem io = new JMenuItem("Input or Output");
+        JMenuItem var = new JMenuItem("Variable Declaration");
+        JMenuItem condition = new JMenuItem("Condition");
+
+
+
         menuBar.add(menu);
+        menuBar.add(menuHelp);
+        menuBar.add(shapes);
+
         menu.add(clear);
         menu.add(save);
         menu.add(load);
+
+        shapes.add(method);
+        shapes.add(instruction);
+        shapes.add(io);
+        shapes.add(var);
+        shapes.add(condition);
+
         setJMenuBar(menuBar);
 
         clear.addActionListener(control);
         save.addActionListener(control);
         load.addActionListener(control);
+        method.addActionListener(control);
+        instruction.addActionListener(control);
+        io.addActionListener(control);
+        var.addActionListener(control);
+        condition.addActionListener(control);
 
 
     }
 
     public static void main(String[] args) {
         Main main = new Main();
+        Repository.getInstance().add(new Circle(30, 30, Color.LIGHT_GRAY, "Begin", Color.BLACK));
+        Repository.getInstance().add(new Circle(450, 410, Color.BLACK, "End", Color.WHITE));
         main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main.setSize(500, 500);
         main.setVisible(true);
--- src/Repository.java
+++ src/Repository.java
@@ -3,7 +3,9 @@ import java.util.List;
 
 public class Repository {
 
-    private List<Node> nodeList = new ArrayList<>();
+    private List<Shape> shapeList = new ArrayList<>();
+    private List<Rectangle> rectList = new ArrayList<>();
+    private List<ConnectingLine> lineList = new ArrayList<>();
     private static Repository repo;
 
     public static Repository getInstance(){
@@ -12,13 +14,35 @@ public class Repository {
         return repo;
     }
 
-    public void add (Node node){
-        nodeList.add(node);
+    public void add (Shape shape){
+        shapeList.add(shape);
     }
 
-    public void clear(){
-        nodeList.clear();
+    public void add (Rectangle rect){rectList.add(rect);}
+    public void add (ConnectingLine line){
+        lineList.add(line);
     }
+
+    public Shape getShape(int index){ return shapeList.get(index);}
+
+    public Rectangle getRect(int index) {return rectList.get(index);}
+
+    public ConnectingLine getLine(int index) {return lineList.get(index);}
+
+    public int shapeSize(){ return shapeList.size();}
+    public int rectSize(){ return rectList.size();}
+    public int lineSize(){ return lineList.size();}
+
+
+    public void clear(){
+        shapeList.clear();
+        rectList.clear();
+        lineList.clear();
+    }
+
+
+
+
 
 
 
