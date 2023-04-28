8c67fd1b8dcc3ce3326d2d344eec6b89a9e5bf9a
jbalikov <jbalikov@calpoly.edu>
2023-04-28 12:46:37 -0700

    Lowered cc of RectangleStandard.java from 105 to 25

--- src/RectangleStandard.java
+++ src/RectangleStandard.java
@@ -31,40 +31,24 @@ public class RectangleStandard extends Shape implements Rectangle{
         g.fillRect(x1-40, y1-40, 80, 80);
         if(label != null){
             g.setColor(Color.BLACK);
-            if(label != null) {
-                if(label.length()>33){
-                    if(label.charAt(11)!=' ' && label.charAt(10)!=' ')
-                        g.drawString(label.substring(0, 11)+"-", x1 - 40 + ((80) / 8), y1-15);
-                    else
-                        g.drawString(label.substring(0, 11), x1 - 40 + ((80) / 8), y1-15);
-                    if(label.charAt(22)!=' ' && label.charAt(21)!=' ')
-                        g.drawString(label.substring(11, 22)+"-", x1 - 40 + ((80) / 8), y1-5);
-                    else
-                        g.drawString(label.substring(11, 22), x1 - 40 + ((80) / 8), y1-5);
-                    if(label.charAt(33)!=' ' && label.charAt(32)!=' ')
-                        g.drawString(label.substring(22, 33)+"-", x1 - 40 + ((80) / 8), y1+5);
-                    else
-                        g.drawString(label.substring(22, 33), x1 - 40 + ((80) / 8), y1+5);
-                    g.drawString(label.substring(33), x1 - 40 + ((80) / 8), y1+15);
-                }else if(label.length()>22){
-                    if(label.charAt(11)!=' ' && label.charAt(10)!=' ')
-                        g.drawString(label.substring(0, 11)+"-", x1 - 40 + ((80) / 8), y1-10);
-                    else
-                        g.drawString(label.substring(0, 11), x1 - 40 + ((80) / 8), y1-10);
-                    if(label.charAt(22)!=' ' && label.charAt(21)!=' ')
-                        g.drawString(label.substring(11, 22)+"-", x1 - 40 + ((80) / 8), y1);
-                    else
-                        g.drawString(label.substring(11, 22), x1 - 40 + ((80) / 8), y1);
-                    g.drawString(label.substring(22), x1 - 40 + ((80) / 8), y1+10);
-                }else if(label.length()>11){
-                    if(label.charAt(11)!=' ' && label.charAt(10)!=' ')
-                        g.drawString(label.substring(0, 11)+"-", x1 - 40 + ((80) / 8), y1-5);
-                    else
-                        g.drawString(label.substring(0, 11), x1 - 40 + ((80) / 8), y1-5);
-                    g.drawString(label.substring(11), x1 - 40 + ((80) / 8), y1+5);
-                }else{
-                    g.drawString(label, x1 - 40 + ((80) / 8), y1);
+            if (label != null) {
+                int maxLength = 11;
+                int y = y1-20;
+                int i;
+                for (i = 0; i + maxLength < label.length(); i += maxLength) {
+                    String segment = label.substring(i, i+maxLength);
+                    if (!segment.endsWith(" ") && label.charAt(i+maxLength)!=' ') {
+                        segment = segment + "-";
+                    }
+                    if(segment.startsWith(" "))
+                        segment = segment.substring(1);
+                    g.drawString(segment, x1 - 40 + ((80) / 8), y);
+                    y += 10;
                 }
+                String finalSegment = label.substring(i);
+                if(finalSegment.startsWith(" "))
+                    finalSegment = finalSegment.substring(1);
+                g.drawString(finalSegment,x1 - 40 + ((80) / 8), y);
             }
         }
     }
